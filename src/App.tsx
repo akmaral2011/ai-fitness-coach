d7e9d5ad8c1f6fa9d8bf37c286ab2de2b05fdd52
Akmaral Tursunbaeva <akmaralmt20@gmail.com>
2026-04-19 18:50:32 +0600

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import './index.css';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
-function MyApp() {
-  return <h1>Hello, React!</h1>
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 const container = document.getElementById('react-root');
-const root = createRoot(container);
-
-root.render(<MyApp />);
+const root = createRoot(container!);
+root.render(<App />);
